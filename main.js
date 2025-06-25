1fe1aa7e61afe1916024bbda0fb0ae10896eeb4c
Junsoo-HUR <obeyhur@gmail.com>
2025-06-25 14:48:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import { firebaseConfig, FLAVORS, SYNERGY_SCORES, CONFLICT_SCORES, CRAFTING_RECIPES, TUTORIAL, DOM_IDS } from './game-data.js';
-import { renderFlavorGrid, updateFlavorGridSelection, confirmFlavorSelection, ... } from './ui-manager.js';
-// ... (Firebase 초기화, 인증, 데이터 로드, 핵심 게임 로직 등) ...
+import * as UIManager from './ui-manager.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, collection, query, orderBy, limit, onSnapshot } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+// ... (이하 모든 게임 로직 관련 함수) ...
